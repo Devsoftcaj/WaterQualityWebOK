d21871de4f6dccd047505a6598e8f99bd9820353
cmartosreyes <cmartosreyes@gmail.com>
2021-06-25 18:11:18 +0200

    Web Completa

--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -9,15 +9,8 @@ import { MapService } from 'src/app/services/map.service';
 const iconRetinaUrl = './assets/marker-icon-2x.png';
 const iconUrl = './assets/marker-icon.png';
 const shadowUrl = './assets/marker-shadow.png';
-const iconDefault = L.icon({
-iconRetinaUrl,
-iconUrl,
-shadowUrl,
-iconSize: [25, 41],
-iconAnchor: [12, 41],
-popupAnchor: [1, -34],
-tooltipAnchor: [16, -28],
-shadowSize: [41, 41]
+const iconDefault = L.icon({iconRetinaUrl,iconUrl,shadowUrl,
+iconSize: [25, 41],iconAnchor: [12, 41],popupAnchor: [1, -34],tooltipAnchor: [16, -28],shadowSize: [41, 41]
 });
 
 
